063127812778cbd8c0c3835793f45cef6a78be62
Ademola Plumptre <dplumptre@yahoo.com>
2021-02-23 14:35:30 +0100

    update

--- src/Container/OpenAccount.js
+++ src/Container/OpenAccount.js
@@ -3,18 +3,23 @@ import { Link } from 'react-router-dom';
 import MyAlert from '../FormValidation/MyAlert';
 import formValidation  from'../FormValidation/StandardForm';
 //import FormAlert from '../Partials/FormAlert';
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const OpenAccount = props => {
 
 
   const v = new formValidation();
+
+   
+
+
   const [state, setState] = useState({
     myForm: {
         fullname: '',
         accounttype: 'Individual',
         email: '',
-        dob: '',
+        dob: new Date(),
         phone: '',
         address: '',
         city: '',
@@ -23,11 +28,13 @@ const OpenAccount = props => {
         postalcode: '',
         bank: '',
         account: '',
-        date_open_account: '',
-        request_date: '',
+        date_open_account: new Date(),
+        request_date: new Date(),
         gender: 'Male',
         company_reg: '',
+        company_reg_date: new Date(),
         contact_person: '',
+        citizen: 'NG',
         bvn: '',
         ref_no: '',
         lga: '',
@@ -49,8 +56,10 @@ const OpenAccount = props => {
         request_date: '',
         gender: '',
         company_reg: '',
+        company_reg_date: '',
         contact_person: '',
         bvn: '',
+        citizen: '',
         ref_no: '',
         lga: '',
     },
@@ -74,9 +83,94 @@ const OpenAccount = props => {
   }
 
 
+  const DOB = date => {
+    setState({ 
+      ...state,
+      myForm:{
+        ...state.myForm,
+        dob: date
+      }
+    });
+ }
+
+
+
+  const changeDateOpenAccount = date => {
+     setState({ 
+       ...state,
+       myForm:{
+         ...state.myForm,
+         date_open_account: date
+       }
+     });
+  }
+
+  const requestDate = date => {
+    setState({ 
+      ...state,
+      myForm:{
+        ...state.myForm,
+        request_date: date
+      }
+    });
+ }
+
+ const companyRegDate = date => {
+  setState({ 
+    ...state,
+    myForm:{
+      ...state.myForm,
+      company_reg_date: date
+    }
+  });
+}
+
+
+
+  const getMyDate = (currentDate) =>{
+    const date = currentDate.getDate();
+    const month = currentDate.getMonth(); //Be careful! January is 0 not 1
+    const year = currentDate.getFullYear();
+  return  year + "-" +('0' + (month + 1)).slice(-2) + "-" +('0' + date).slice(-2);
+  }
+
+
+
+
+
+
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    console.log(state);
+
+    const formData = {
+      fullname: state.myForm.fullname,
+      accounttype: state.myForm.accounttype,
+      email: state.myForm.email,
+      dob: getMyDate(state.myForm.dob),
+      phone: state.myForm.phone,
+      address: state.myForm.address,
+      city: state.myForm.city,
+      your_state:state.myForm.your_state,
+      country: state.myForm.country,
+      postalcode: state.myForm.postalcode,
+      bank: state.myForm.bank,
+      account: state.myForm.accounttype,
+      date_open_account: getMyDate(state.myForm.date_open_account),
+      request_date: getMyDate(state.myForm.request_date),
+      gender: state.myForm.gender,
+      company_reg:state.myForm.company_reg,
+      company_reg_date: getMyDate(state.myForm.company_reg_date),
+      contact_person: state.myForm.contact_person,
+      bvn: state.myForm.bvn,
+      ref_no: state.myForm.ref_no,
+      lga: state.myForm.lga,
+    }
+
+
+
+
+    console.log(formData);
   }
 
   // const FORM_CURRENT_INPUT = "FORM_CURRENT_INPUT";
@@ -233,9 +327,14 @@ const OpenAccount = props => {
 
             <div className="form-row">
               <div className="form-group col-md-6">
-                <label >Date of Birth</label>
-                <input name="dob" value={state.myForm.dob} type="text" onChange={onchangeHandler} className="form-control" />
-                
+                <label style={{display:'block'}}>Date of Birth</label>
+               <DatePicker
+                      selected={state.myForm.dob}
+                      className=" mb-2 form-control" 
+                      name="dob"
+                      dateFormat="yyyy-MM-dd"
+                      onChange={date =>DOB(date)}
+                    />
               </div>
               <div className="form-group col-md-6">
                 <label >Phone</label>
@@ -295,15 +394,25 @@ const OpenAccount = props => {
 
             <div className="form-row">
               <div className="form-group col-md-6">
-                <label >Date of Opening Account</label>
-                <input type="text" value={state.myForm.date_open_account} className="form-control" onChange={onchangeHandler} name="date_open_account" />
-               
+                <label  style={{display:'block'}}>Date of Opening Account</label>
+                <DatePicker
+                      selected={state.myForm.date_open_account}
+                      className="  form-control" 
+                      name="date_open_account"
+                      dateFormat="yyyy-MM-dd"
+                      onChange={date =>changeDateOpenAccount(date)}
+                    />
               </div>
 
               <div className="form-group col-md-6">
-                <label >Request Date</label>
-                <input name="request_date" type="text" value={state.myForm.request} onChange={onchangeHandler} className="form-control" />
-                 
+                <label style={{display:'block'}}>Request Date</label>
+               <DatePicker
+                      selected={state.myForm.request_date}
+                      className="  form-control" 
+                      name="request_date"
+                      dateFormat="yyyy-MM-dd"
+                      onChange={date =>requestDate(date)}
+                    />       
               </div>
             </div>
 
@@ -325,9 +434,10 @@ const OpenAccount = props => {
               </div>
               <div className="form-group col-md-2">
                 <label>Citizen</label>
-                <select name="citizen" id="inputState" onChange={onchangeHandler} className="form-control">
-                  <option >Choose...</option>
-                  <option>NG</option>
+                <select name="citizen"  onChange={onchangeHandler} className="form-control">
+                  <option value="NG">NG</option>
+                  <option value="USA">USA</option>
+                  <option value="UK">UK</option>
                 </select>
               </div>
             </div>
@@ -343,9 +453,14 @@ const OpenAccount = props => {
                 <MyAlert error={state.formErrors.contact_person}  />   
               </div>
               <div className="form-group col-md-6">
-                <label >Company Registration Date</label>
-                <input type="text" name="company_reg_date" value={state.myForm.company_reg_date} onChange={onchangeHandler} className="form-control" />
-                   
+                <label style={{display:'block'}} >Company Registration Date</label>
+               <DatePicker
+                      selected={state.myForm.company_reg_date}
+                      className=" form-control" 
+                      name="company_reg_date"
+                      dateFormat="yyyy-MM-dd"
+                      onChange={date =>companyRegDate(date)}
+                    />
               </div>
               <div className="form-group col-md-2">
                 <label >BVN</label>
@@ -362,7 +477,7 @@ const OpenAccount = props => {
               </div>
               <div className="form-group col-md-4">
                 <label >State</label>
-                <input name="state" type="text" value={state.myForm.your_state} onChange={onchangeHandler} className="form-control" />
+                <input name="your_state" type="text" value={state.myForm.your_state} onChange={onchangeHandler} className="form-control" />
                 <MyAlert error={state.formErrors.your_state}  /> 
               </div>
               <div className="form-group col-md-4">
@@ -371,8 +486,7 @@ const OpenAccount = props => {
                 <MyAlert error={state.formErrors.lga}  />   
               </div>
             </div>
-
-            <button type="submit" onClick={onSubmitHandler.bind(this)} className="btn btn-primary">Submit</button>
+            <button disabled={!v.isformValid(state.myForm,state.formErrors)}   type="submit" onClick={onSubmitHandler.bind(this)} className="btn btn-primary">Submit</button>
           </form>
         </div>
       </div>
